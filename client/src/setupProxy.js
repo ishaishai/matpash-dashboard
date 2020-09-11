40ef51abb52512b41a1779704c1f96e388a76cae
Stav Elmashally <stavelmashally6@gmail.com>
2020-09-11 19:34:08 +0300

    proxy api calls

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
+  // Send api calls to the api server
   app.use(
     '/api/*',
     createProxyMiddleware({
