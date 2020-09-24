8af34b06789dcdfa9f49191bbaf5900c3a94bffa
Stav Elmashally <stavelmashally6@gmail.com>
2020-09-24 18:43:00 +0300

    login page error handling

--- client/src/components/Login/custom-button/custom-button.component.js
+++ client/src/components/Login/custom-button/custom-button.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './custom-buttom.styles.scss';
 
 const CustomButton = ({ children, ...otherProps }) => (
-  <button className='custom-button' {...otherProps}>
+  <button className="custom-button ui white button" {...otherProps}>
     {children}
   </button>
 );
--- client/src/components/Login/sign-in/sign-in.component.js
+++ client/src/components/Login/sign-in/sign-in.component.js
@@ -1,71 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../../actions';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      UserName: '',
-      Password: '',
-    };
-  }
-  handleSubmit = event => {
+const SignIn = ({ login, error }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = event => {
     event.preventDefault();
-    this.setState({ UserName: '', Password: '' });
-  };
-  handlechange = event => {
-    const { value, name } = event.target;
-
-    this.setState({ [name]: value });
-  };
-  senduserdetails = () => {
-    const user = {
-      username: this.state.UserName,
-      password: this.state.Password,
-    };
-
-    this.props.login(user);
+    setUsername('');
+    setPassword('');
   };
 
-  render() {
-    // if (this.props.error.msg) {
-    //   alert(this.props.error.msg);
-    //   this.props.clearErrors();
-    // }
-    return (
-      <div style={{ height: '100vh' }} className="image">
-        <div className="sign-in">
-          <h1> ברוכים הבאים </h1>
-          <form onSubmit={this.handleSubmit}>
+  return (
+    <div style={{ height: '100vh' }} className="image">
+      <div className="sign-in ui big form">
+        <h1> ברוכים הבאים </h1>
+        <form onSubmit={handleSubmit}>
+          <div class={`field ${error.msg ? 'error' : ''}`}>
             <FormInput
               name="UserName"
               type="text"
-              handleChange={this.handlechange}
-              value={this.state.UserName}
+              handleChange={e => setUsername(e.target.value)}
+              value={username}
               placeholder="שם משתמש"
               required
             />
+          </div>
+          <div class={`field ${error.msg ? 'error' : ''}`}>
             <FormInput
               name="Password"
               type="password"
-              handleChange={this.handlechange}
-              value={this.state.Password}
+              handleChange={e => setPassword(e.target.value)}
+              value={password}
               placeholder="סיסמא"
               required
             />
-            <CustomButton type="submit" onClick={this.senduserdetails}>
-              {' '}
-              כניסה{' '}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <CustomButton
+              type="submit"
+              onClick={() => login({ username, password })}
+            >
+              כניסה
             </CustomButton>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default connect(null, { login })(SignIn);
+const mapStateToProps = ({ error }) => ({ error });
+
+export default connect(mapStateToProps, { login })(SignIn);
