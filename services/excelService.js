61bf22e2af300b22276abf8729c7200cdbaf5afc
Ishai Yaish <36458741+ishaishai@users.noreply.github.com>
2020-11-28 10:47:47 +0200

    prototype prod v1

--- services/excelService.js
+++ services/excelService.js
@@ -7,13 +7,14 @@ class ExcelService {
       const python = spawn('python', ['excelCheck.py', path, filename]);
       let error;
       let result;
-
       python.stdout.on('data', data => {
         result = data.toString();
+        console.log(result);
       });
 
       python.stderr.on('data', data => {
         error = data.toString();
+        console.log(error);
       });
 
       python.stdout.on('end', () => {
